0786ddc66a5c0031a72a2ad3dd8e54daa09a0f87
sunil-dhi <sunildhimanmatharu@gmail.com>
2023-09-25 17:55:02 -0400

    client folder uploading

---
+++ client/src/CreateUser.jsx
@@ -0,0 +1,72 @@
+import axios from "axios";
+import { useState } from "react";
+import { addUser } from "./redux/userSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+function CreateUser() {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [age, setAge] = useState();
+  // const [phone,setPhone]=useState();
+  // const [gender,setGender]=useState()
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3001/create", { name, email, age })
+      .then((res) => {
+        dispatch(addUser(res.data));
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className="w-50 bg-white rounded p-3">
+        <form onSubmit={handleSubmit}>
+          <h2>Add User</h2>
+          <div className="mb-2">
+            <label htmlFor="">Name</label>
+            <input
+              type="text"
+              placeholder="Enter Name"
+              required="true"
+              className="form-control"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="">Email</label>
+            <input
+              type="email"
+              required="true"
+              placeholder="Enter Email"
+              className="form-control"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="">Age</label>
+            <input
+              type="text"
+              placeholder="Enter Age"
+              required="true"
+              className="form-control"
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </div>
+         
+          
+          <button className="btn btn-success">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default CreateUser;
